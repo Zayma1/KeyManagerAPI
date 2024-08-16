bc5b72064e288bc7569ce3dd9742a82468f09aad
zayma1 <zaymakinsiona@gmail.com>
2024-08-16 10:01:30 -0300

    Added Spring files

---
+++ src/test/java/com/manager/KeyManager/KeyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.KeyManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
